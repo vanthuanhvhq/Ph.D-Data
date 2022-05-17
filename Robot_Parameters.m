738b6ed2929fa547c1e4a272e1c76d777d50c68c
PHAM VAN THUAN <54723476+vanthuanhvhq@users.noreply.github.com>
2022-05-17 09:17:26 +0200

    Add files via upload

---
+++ Robot_Parameters.m
@@ -0,0 +1,15 @@
+%DC MOTOR PARAMETERS
+L = 0.01;   % The electrical inductance 
+R = 0.7;    % The electrical resistance 
+Kt = 0.001; % The torque constant
+Ke = 0.03;  % The back emf constant 
+B = 0.004;  % The damping ratio
+J = 0.0363; % The moment of inertia of the rotor
+
+%ROBOT CHASSIS PARAMETERS
+N = 30;     %The gear ratio
+r = 0.0425; % The radius of wheel
+a = 0;      % The distance between G and Q
+b = 0.2;    % The distance between two diving wheel
+m = 1.733;  % The mass of robot
+I = 0.08;   % Moment inertia of robot 
